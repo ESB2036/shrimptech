a6b1a9f5d6dbab35c20da7dbba4daf6cc39bfdc1
Emeg21 <emelyobandog@gmail.com>
2025-11-18 14:01:03 -0500

    Update alerts.ts

--- src/app/modules/inventory/components/alerts/alerts.ts
+++ src/app/modules/inventory/components/alerts/alerts.ts
@@ -1,1 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alerts',
+  imports: [],
+  templateUrl: './alerts.html',
+  styleUrl: './alerts.css',
+})
+export class Alerts {}
 
